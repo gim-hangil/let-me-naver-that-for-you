456b7f41a0344046899d1a6bc3222ebd27381808
gim-hangil <gim_hangil@kakao.com>
2022-10-26 23:43:56 +0900

    :sparkles: 쿼리가 있는지에 따라 검색 버튼 혹은 공유 버튼이 표시됨

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
 import { ref } from "vue";
+import { getQuery } from "@/helpers";
 import SearchButton from "./components/SearchButton.vue";
 import ShareButton from "./components/ShareButton.vue";
 
 const query = ref("");
+const isSharedUrl = getQuery() === null || getQuery().trim().length === 0;
 </script>
 
 <template>
   <input v-model="query" />
-  <ShareButton :query="query" />
-  <SearchButton />
+  <ShareButton v-if="isSharedUrl" :query="query" />
+  <SearchButton v-else />
 </template>
 
 <style scoped></style>

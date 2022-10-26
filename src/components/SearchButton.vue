5fe4c720d2d90489d73cc6d5e2526f6d1fe10317
gim-hangil <gim_hangil@kakao.com>
2022-10-26 23:32:58 +0900

    :lipstick: `SearchButton`에 스타일 및 아이콘 적용

--- src/components/SearchButton.vue
+++ src/components/SearchButton.vue
@@ -1,4 +1,6 @@
 <script setup>
+import search from "@/assets/search.svg";
+
 function getQuery() {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get("q");
@@ -10,7 +12,24 @@ function redirectToNaver() {
 </script>
 
 <template>
-  <button @click="redirectToNaver">검색</button>
+  <button @click="redirectToNaver">
+    <img :src="search" />
+  </button>
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  width: 56px;
+  height: 56px;
+  color: white;
+  background-color: #03cf5d;
+  border: none;
+  border-radius: 0 2px 2px 0;
+  cursor: pointer;
+}
+
+img {
+  width: 22px;
+  height: 22px;
+}
+</style>

faa2263c37eaace75eb1144646d87f158854b220
gim-hangil <gim_hangil@kakao.com>
2022-10-27 00:34:32 +0900

    :sparkles: 빈 검색어를 입력하면 alert가 아닌 복사 알림과 같은 붉은 label이 표시됨

--- src/components/ShareButton.vue
+++ src/components/ShareButton.vue
@@ -4,18 +4,20 @@ import { getSharingUrl } from "@/helpers";
 import share from "@/assets/share.svg";
 
 const props = defineProps({ query: String });
+const emptyQuery = ref(false);
 const hasCopied = ref(false);
 
 watch(
   () => props.query,
   () => {
+    emptyQuery.value = false;
     hasCopied.value = false;
   }
 );
 
 function copySharingUrl(q) {
   if (q.trim().length === 0) {
-    window.alert("⚠️ 검색어를 입력해주세요");
+    emptyQuery.value = true;
   } else {
     navigator.clipboard.writeText(getSharingUrl(q));
     hasCopied.value = true;
@@ -27,7 +29,12 @@ function copySharingUrl(q) {
   <button name="button" @click="copySharingUrl(props.query)">
     <img :src="share" />
   </button>
-  <label v-if="hasCopied" class="good" for="button">링크를 복사했습니다.</label>
+  <label v-if="hasCopied" class="good" for="button">
+    링크를 복사했습니다.
+  </label>
+  <label v-if="emptyQuery" class="warning" for="button">
+    검색어를 입력해주세요.
+  </label>
 </template>
 
 <style scoped>
@@ -51,6 +58,11 @@ label {
 label.good {
   background-color: var(--light-green);
   color: var(--dark-green);
+}
+
+label.warning {
+  background-color: var(--light-red);
+  color: var(--dark-red);
 }
 
 img {
